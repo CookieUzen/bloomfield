fcc9129c01981d34a6f38740ef29500ce11b695f
Uzen Huang <uzen@cookieuz.io>
2025-04-14 22:24:08 -0500

    added tomatos

--- input.js
+++ input.js
@@ -141,9 +141,9 @@ export default class InputScene extends Phaser.Scene {
         });
 
 		// Bind keys 1-9 for selecting tools
-		const keys = ['Q', 'W', 'E', 'ONE', 'TWO', 'THREE', 'FOUR'];
+		const keys = ['Q', 'W', 'E', 'R', 'ONE', 'TWO', 'THREE', 'FOUR'];
 		const tools = [{type: ToolTypes.EQUIPMENT, toolName: 'watering_can'}, {type: ToolTypes.EQUIPMENT, toolName: 'sickle'}, {type: ToolTypes.EQUIPMENT, toolName: 'fertilizer'}, 
-			{type: ToolTypes.SEED, toolName: 'corn'}, {type: ToolTypes.SEED, toolName: 'soybean'}, {type: ToolTypes.SEED, toolName: 'potato'}, {type: ToolTypes.SEED, toolName: 'wheat'}
+			{type: ToolTypes.SEED, toolName: 'corn'}, {type: ToolTypes.SEED, toolName: 'soybean'}, {type: ToolTypes.SEED, toolName: 'potato'}, {type: ToolTypes.SEED, toolName: 'tomato'}, {type: ToolTypes.SEED, toolName: 'wheat'}
 		]
 		keys.forEach((key, index) => {
 
